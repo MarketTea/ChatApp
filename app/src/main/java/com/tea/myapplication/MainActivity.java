642cbb8a64e4ed19f934363995626473eb2ad1b0
nducthien <thien.chotrax23@gmail.com>
2021-07-05 11:27:01 +0700

    add library and permission

---
+++ app/src/main/java/com/tea/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.tea.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static int LOGIN_REQUEST_CODE = 7171;
+    private List<AuthUI.IdpConfig> providers;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener listener;
+
+    FirebaseDatabase database;
+    DatabaseReference userRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
